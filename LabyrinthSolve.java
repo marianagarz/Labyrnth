51fc7f90770383acbd267610ae70b2f06f5e4e69
Garza <mgarza@friendsbalt.org>
2021-12-08 13:11:43 -0500

    after help w mr hammer

---
+++ LabyrinthSolve.java
@@ -0,0 +1,12 @@
+public class LabyrinthSolve {
+	pubic int n;
+	private int[] solve(Labyrinth L) {
+		//solve the maze!
+		//using backtracing 
+	}
+	
+	public void printSolution(int n){
+		int[] sol= new int[n]
+		for 
+	}
+}
