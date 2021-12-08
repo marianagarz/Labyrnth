913b3a0e9d23c75c6d978be9ab72ea20609be8a6
Garza <mgarza@friendsbalt.org>
2021-12-08 13:11:08 -0500

    after help w mr. hammer

--- solveMaze.java
+++ solveMaze.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 public class solveMaze{
 	
 	//need some help with how big everything should be 
+	//. rows and. cols to find the last spot for the base case 0-rows-1 0-cols-1
 	int rowNum, collNum;
 	boolean[][] tested= new boolean[rowNum][collNum]; 
 	
@@ -10,7 +11,7 @@ public class solveMaze{
 	
 	public int[] solve (Labrynth l)
 	{
-		findSafeMove(x,y,l);//SOS what shoud my x and ys b? seems like they cant b zero ciz wouldnt that get messed up?
+		findSafeMove(0,0,l);//SOS what shoud my x and ys b? seems like they cant b zero ciz wouldnt that get messed up?
 		int[] solutions= new int[moves.size()];
 		for (n=0, n<moves.size(),n--) //am i chopping stuff off or about to run into a huge problem here?
 			solutions[n]=moves.get(n); //correct syntax??
@@ -20,20 +21,20 @@ public class solveMaze{
 	
 	public static boolean findSafeMove(int x, int y, Labrynth l)
 	{
-		//base case. QUESTION: how to know when you are at the end
+		//base case. QUESTION: how to know when you are at the end x= lbrynth.rows-1 labrynth.col-1
 		if ((x==rowNum-1)&&(y==collNum-1))
 			return true;
-		for (n=4;n>0;n--){
-			//checking square up-- QUESTION: is this nessesary? feels to me like you  wouldn't need to check up when solving a maze...
-			if  (isSafe(x,y-1,l)
+		for (n=0;n<4;n--){
+			//checks up go through and change to row and coll 
+			if  (isSafe(x,y-1,l))
 			{
 				moves.add(0); 
-				tested[x][y-1]=true; 
+				tested[x][y-1]=true; //CHECK ROWS ETC 
 				//ik you need to test the next one--> do you need to  check everysingle next one- in all directions? or no
-				if (findSafeMove(x,y-1,l)
+				if (findSafeMove(x,y-1,l)) // same as whatever you jist did 
 					return true; 
 				else 
-					goBack(x,y-1); 
+					goBack(x,y-1); //SAME AS BEFORE 
 			}
 			
 			//checking square down 
@@ -72,14 +73,14 @@ public class solveMaze{
 	
 	public static void goBack(int x,int y)
 	{
-		moves.remove(moves.size());
+		moves.remove(moves.size()-1);
 		tested[x][y]= false; 
 	}
 	
 	
 	public boolean isSafe(int x, int y, Labrynth l) 
 	{
-		if ((l.isValid(x,y)&&l.isStone(x,y))&&(!hasBeentested(x,y)))
+		if ((l.isValid(x,y)&&l.isStone(x,y))&&(!hasBeentested(x,y))) 
 			return true; 
 	} 
 	
